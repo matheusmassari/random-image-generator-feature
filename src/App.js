1258214aeb81be340c50dd85238e30a87ab9c9e8
Matheus Massari <83505673+matheusmassari@users.noreply.github.com>
2021-09-22 16:13:19 -0300

    app versao 1.1 - useState para armazenar a resposta da API externa, favoritos no localStorage, useEffect para rodar a primeira imagem e setItem no localStorage monitorando mudancas na variavel favorites

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import "./App.css";
-
-async function getExternalImage() {
-  const response = await fetch("https://source.unsplash.com/random");
-
-  document.querySelector(".image").innerHTML = `<img src = "${response.url}">`;
-}
+import { useState, useEffect } from "react";
 
 
 function App() {
+  const [response, setResponse] = useState({});
+  const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem('favorites')) || [])
+  
+  useEffect(() => {
+    getExternalImage()
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  },[favorites])
+
+  async function getExternalImage() {
+    const response = await fetch("https://source.unsplash.com/random");
+    setResponse(response)
+  }
+
+  function handleFavorites() {
+    const imageSource = response.url
+    setFavorites((atual) => [...atual, imageSource])
+  }
+  
   return (
     <main>
       <button onClick={getExternalImage}>Generate</button>
       <div className="image">
-        
+        <img onClick={handleFavorites} src={response.url} alt="imagem" />
       </div>
     </main>
   );
