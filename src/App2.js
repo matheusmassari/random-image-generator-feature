8acf2f351d6c1b4e028c3aaae739b28678adbe9a
Matheus Massari <83505673+matheusmassari@users.noreply.github.com>
2021-09-26 21:20:55 -0300

    versao 2.0

--- src/App2.js
+++ src/App2.js
@@ -2,20 +2,70 @@ import { useState, useEffect } from "react";
 import "./App2.css";
 
 function App2() {
-  const [resposta, setResposta] = useState({});
+  const [resposta, setResposta] = useState([]);
+  const [username, setUsername] = useState("matheusmassari");
+  const [loading, setLoading] = useState(false);
 
-  async function getInfoGithub(username = `matheusmassari`) {
-    const response = await fetch(`https://api.github.com/users/${username}/repos`);
-    response.json().then((body) => setResposta(body));
+  async function getInfoGithub(username) {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/${username}/repos`
+      );
+      setLoading(false);
+      response.json().then((body) => setResposta(body));
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
   }
+
+  function handleUsername(event) {
+    event.preventDefault();
+    const userInput = event.target.value;
+    setUsername(userInput);
+    console.dir(userInput);
+  }
+
   useEffect(() => {
-    getInfoGithub();
+    getInfoGithub(username);
   }, []);
-  
-  console.log(resposta)
-  return <div className="userInfo">
-     {''}
-  </div>
+
+  console.log(resposta);
+  return (
+    <>
+      <div className="container">
+        <div className="label-input-container">
+          <label htmlFor="username">Usuario: </label>
+          <input
+            placeholder="userid"
+            id="username"
+            onChange={handleUsername}
+            type="text"
+          />
+        </div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            <div className='image-container'>
+            <img src={resposta[0]?.owner?.avatar_url} alt="" />
+            </div>
+            <ul>
+              {resposta.map((elemento) => (
+                <li className="list-element" key={elemento.id}>
+                  {elemento.name}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+        <button onClick={() => getInfoGithub(username)} type="submit">
+          Buscar
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App2;
